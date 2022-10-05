2d7964a7bf1c37c7cd5643281c19320a1acd1857
zemlianin <detulie@mail.ru>
2022-10-05 10:24:58 +0300

    Normal-gen

--- ClassLibraryPart_1/Vault.cs
+++ ClassLibraryPart_1/Vault.cs
@@ -41,55 +41,18 @@ namespace ClassLibraryPart_1
         /// <exception cref="ArgumentOutOfRangeException">Если номер не лежит в промежутке от 0 до 10</exception>
         public void Download(int number)
         {
-            switch (number)
+            if (number > 0)
             {
-                case 0:
-                    Load0(this);
-                    break;
-                case 1:
-                    Load1(this);
-                    break;
-                case 2:
-                    Load2(this);
-                    break;
-                case 3:
-                    Load3(this);
-                    break;
-                case 4:
-                    Load4(this);
-                    break;
-                case 5:
-                    Load5(this);
-                    break;
-                case 6:
-                    Load6(this);
-                    break;
-                case 7:
-                    Load7(this);
-                    break;
-                case 8:
-                    Load8(this);
-                    break;
-                case 9:
-                    Load9(this);
-                    break;
-                default: throw new ArgumentOutOfRangeException(nameof(number));
+                AutoDownload(number, this);
             }
         }
-       /// <summary>
-       /// Методы подгрзуки
-       /// </summary>
-       /// <param name="v">Сссылка на контейнер в который требуется подгрузка</param>
-        static partial void Load0(Vault v);
-        static partial void Load1(Vault v);
-        static partial void Load2(Vault v);
-        static partial void Load3(Vault v);
-        static partial void Load4(Vault v);
-        static partial void Load5(Vault v);
-        static partial void Load6(Vault v);
-        static partial void Load7(Vault v);
-        static partial void Load8(Vault v);
-        static partial void Load9(Vault v);
+        /// <summary>
+        /// Выполнение метода подгрузки
+        /// </summary>
+        /// <param name="number">Номер метода</param>
+        /// <exception cref="ArgumentOutOfRangeException">Если номер не лежит в промежутке от 0 до 10</exception>
+        static partial void AutoDownload(int number, Vault v);
+
         /// <summary>
         /// Поиск по названию
         /// </summary>
